871c9f64d418e58e664dc90186e4fc2f41a851ad
Kai Mareving <karska.joanna@gmail.com>
2021-10-25 17:46:29 +0200

    init app

---
+++ app/script.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { render } from 'react-dom';
+import AppDescription from './AppDescription.js';
+
+class App extends React.Component {
+
+  state = {
+    status: 'off',
+    time: 1200,
+    timer: null,
+  };
+
+  render() {
+    const { status, time } = this.state;
+
+    return (
+      <div>
+        <h1>Protect your eyes</h1>
+        {(status === 'off') && <AppDescription />}
+        {(status === 'work') && <img src="./images/work.png" />}
+        {(status === 'rest') && <img src="./images/rest.png" />}
+        {(status !== 'off') && <div className="timer">18:23</div>}
+        {(status === 'off') && <button className="btn">Start</button>}
+        {(status !== 'off') && <button className="btn">Stop</button>}
+        <button className="btn btn-close">X</button>
+      </div>
+    )
+  }
+};
+
+render(<App />, document.querySelector('#app'));
