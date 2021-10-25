9931273ea1a2cc0ed33ab03758a5ac8511450e53
Kai Mareving <karska.joanna@gmail.com>
2021-10-25 23:57:47 +0200

    Electron App v.0.2 [DONE]

--- app/script.js
+++ app/script.js
@@ -1,20 +1,73 @@
 import React from 'react';
 import { render } from 'react-dom';
 
-
-
 class App extends React.Component {
 
   state = {
     status: 'off',
     time: 1200,
     timer: null,
-  };
+  }
 
+  formatTime(time) {
+    let sec_num = parseInt(time, 10);
+      let min = Math.floor(sec_num / 60) % 60;
+      let sec = sec_num % 60;
+      return [min,sec].map(v => v < '10' ? '0' + v : v).join(':');
+  }
+
+  step = () => {
+    this.setState({
+      time: this.state.time - 1,
+    });
+
+  if (this.state.time === 0) {
+    if (this.state.status === 'work'){
+      this.setState({
+        status: 'rest',
+        time: 20,
+      });
+      this.playBell();
+    } else if (this.state.status === 'rest'){
+      this.setState({
+        status: 'work',
+        time: 1200,
+        });
+        this.playBell();
+      }
+    }
+  }
+
+  startTimer = (e) => {
+    e.preventDefault();
+    this.setState({
+      timer: setInterval(this.step, 1000),
+      time: 1200,
+      status: 'work',
+      });
+  }
+
+  stopTimer = (e) => {
+    e.preventDefault();
+    this.setState({
+      timer: clearInterval(this.step),
+      time: 0,
+      status: 'off',
+    });
+  }
+
+  closeApp = (e) => {
+    e.preventDefault();
+    window.close();
+  }
+
+  playBell = () => {
+    const bell = new Audio('./sounds/bell.wav');
+    bell.play();
+  }
 
   render() {
-
-    const { status, time } = this.state;
+    const { status } = this.state;
 
     const AppDescription = () => {
       return (
@@ -25,73 +78,20 @@ class App extends React.Component {
       );
     };
 
-    const formatTime = (time) => {
-      let sec_num = parseInt(time, 10);
-      let min = Math.floor(sec_num / 60) % 60;
-      let sec = sec_num % 60;
-      return [min,sec].map(v => v < '10' ? '0' + v : v).join(':');
-    };
-
-    const step = () => {
-      let newTime = this.state.time - 1;
-      this.setState({
-        time: newTime,
-      });
-      if (newTime === 0 && this.state.status === 'work') {
-        this.setState({
-          status: 'rest',
-          time: 20,
-        });
-      }
-      else if (newTime === 0 && this.state.status === 'rest') {
-        this.setState({
-          status: 'work',
-          time: 1200,
-        });
-      }
-    };
-
-    const startTimer = () => {
-      this.setState({
-        status: 'work',
-        timer: setInterval(step, 1000),
-      })
-    };
-
-    const stopTimer = () => {
-      this.setState({
-        status: 'off',
-        time: 1200,
-      }),
-        clearInterval(this.state.timer);
-    };
-
-    const closeApp = () => {
-      window.close();
-    };
-
     return (
       <div>
         <h1>Protect your eyes</h1>
-        {(status === 'off') && <AppDescription />}
+        {status === 'off' && <AppDescription />}
         {(status === 'work') && <img src='./images/work.png' />}
         {(status === 'rest') && <img src='./images/rest.png' />}
-        {(status !== 'off') && <div className='timer'>{formatTime(time)}</div>}
-        {
-          (status === 'off') && <button className='btn' onClick={event => {
-            event.preventDefault();
-            return startTimer();
-          }}>Start</button>
+        {(status !== 'off') && <div className='timer'>
+          {this.formatTime(this.state.time)}
+          </div>
         }
-        {(status !== 'off') && <button className='btn' onClick={event => {
-          event.preventDefault();
-          return stopTimer();
-        }}>Stop</button>}
-        <button className='btn btn-close' onClick={event => {
-          event.preventDefault();
-          return closeApp();
-        }}>X</button>
-      </div >
+        {(status === 'off') && <button className='btn' onClick={this.startTimer}>Start</button>}
+        {(status !== 'off') && <button className='btn' onClick={this.stopTimer}>Stop</button>}
+        <button className='btn btn-close'onClick={this.closeApp}>X</button>
+      </div>
     )
   }
 };
